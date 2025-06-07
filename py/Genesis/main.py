13ec6cb297ed2ef786c08d3d996ce2f091a427c1
Joshua Adewole <adewole.josh@gmail.com>
2025-06-07 12:12:02 +0100

    feat: multi-client, event-driven

--- py/Genesis/main.py
+++ py/Genesis/main.py
@@ -10,49 +10,78 @@
 # Though the former was short-lived, closes immediately after sending a response
 # The other continues to handle the response.
 # ---
-# This made me play around into using TCP/UDP but below is just using the TCP (more reliable protocol)
-# as against the UDP (fire and forget guy)
-# Then finally trying to make sense, and remember the OSI model off-hand
-# Has 7 layers: (Application -> Presentation -> Session) -> Transport (TCP/UDP) -> Network (IP) -> Data Link -> Physical
-# pretty interesting.
+# This made me play around into using TCP/UDP but below is just using the TCP (more reliable protocol) [old]
+# as against the UDP (fire and forget guy) [old]
+# Then finally trying to make sense, and remember the OSI model off-hand [old]
+# Has 7 layers: (Application -> Presentation -> Session) -> Transport (TCP/UDP) -> Network (IP) -> Data Link -> Physical [old]
+# pretty interesting. [old]
+# ---
+# switching into multi-client, event driven server
+# we fired up multiple clients connected to the port using 'nc localhost 8080' and sent messages...
+# also keyboard interrupt closed all open connections. GREAT!
 
 import socket
+import select
 
 
 def run_server(host: str = "127.0.0.1", port: int = 8080):
     socket_afn = socket.AF_INET
     socket_stream = socket.SOCK_STREAM
 
-    with socket.socket(socket_afn, socket_stream) as server_socket:
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # with socket.socket(socket_afn, socket_stream) as server_socket:
+    server_socket = socket.socket(socket_afn, socket_stream)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    print(f"> serving on http://{host}:{port} ...")
 
-        server_socket.bind((host, port))
+    # we can non-block here right???
+    server_socket.setblocking(False)
 
-        server_socket.listen(1)
-        print(f"> serving on http://{host}:{port} ...")
+    sockets = [server_socket]
+    try:
+        while True:
+            readable, _, _ = select.select(sockets, [], [])
 
-        try:
-            while True:
-                client_connection, client_address = server_socket.accept()
-
-                with client_connection:
-                    request = client_connection.recv(1024).decode("utf-8")
-
-                    print("here's the request")
-                    print(request)
-
-                    # prepare a response
-                    response = (
-                        "HTTP/1.1 200 OK\r\n"
-                        "Content-Type: text/plain\r\n"
-                        "Content-Length: 13\r\n"
-                        "\r\n"
-                        "HELLO ON THIS SERVER"
+            for sock in readable:
+                if sock is server_socket:
+                    client_sock, client_add = server_socket.accept()
+                    print(
+                        f"> 'we got a match' :) hahha, lol. New Connection from: {client_add}"
                     )
+                    client_sock.setblocking(False)
+                    sockets.append(client_sock)
+                else:
+                    try:
+                        request = sock.recv(1024)
+                        if request:
+                            print("> receiving???")
+                            print(request.decode("utf-8"))
 
-                    client_connection.sendall(response.encode("utf-8"))
-        except KeyboardInterrupt:
-            client_connection.close()
+                            response = (
+                                "HTTP/1.1 200 OK\r\n"
+                                "Content-Type text/plain\r\n"
+                                "Content-Length 20\r\n"
+                                "\r\n"
+                                "HELLO ON THIS SERVER"
+                            )
+
+                            sock.sendall(response.encode("utf-8"))
+                            sockets.remove(sock)
+                            sock.close()
+
+                        else:
+                            sockets.remove(sock)
+                            sock.close()
+
+                    except ConnectionRefusedError:
+                        sockets.remove(sock)
+                        sock.close()
+
+    except KeyboardInterrupt:
+        print("\n\r>> absolutely shutting down!")
+        for sock in sockets:
+            sock.close()
 
 
 if __name__ == "__main__":
