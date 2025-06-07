e631cb435f96eb9b70503876aacfe8352137cc6d
Joshua Adewole <adewole.josh@gmail.com>
2025-06-07 18:29:27 +0100

    feat: using asyncio which uses selectors which uses select

--- py/Genesis/main.py
+++ py/Genesis/main.py
@@ -25,66 +25,57 @@
 # maybe explore how `selectors` work under the hood in python???
 # just went to the library, it feels like a bunch of classes that listen to the fileobj you pass in
 # and uses select to handle them gracefully. pretty cool stuff!
+# ---
+# moving on to asyncios :) pretty excited about this one
+# a code-overhaul
 
-import socket
-import selectors
+import asyncio
+from typing import Any
 
 
-def run_server(host: str = "127.0.0.1", port: int = 8080):
-    S = selectors.DefaultSelector()
-
-    def accept(socket: socket.socket):
-        client_conn, client_address = socket.accept()
-        print(f"> New connection from address: {client_address}")
-        client_conn.setblocking(False)
-        S.register(client_conn, selectors.EVENT_READ, read)
-
-    def read(conn: socket.socket):
-        try:
-            data = conn.recv(1024)
-
-            if data:
-                print("> requesting")
-                print(data.decode("utf-8"))
-
-                response = (
-                    "HTTP/1.1 200 OK\r\n"
-                    "Content-Type text/plain\r\n"
-                    "Content-Length 20\r\n"
-                    "\r\n"
-                    "HELLO ON THIS SERVER"
-                )
-
-                conn.sendall(response.encode("utf-8"))
-            S.unregister(conn)
-            conn.close()
-
-        except ConnectionResetError:
-            S.unregister(conn)
-            conn.close()
-
-    server_socket = socket.socket()
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((host, port))
-    server_socket.listen()
-    server_socket.setblocking(False)
-
-    S.register(server_socket, selectors.EVENT_READ, accept)
-
-    print(f"> serving on http://{host}:{port} ...")
+async def handle_client(reader: Any, writer: Any):
+    addr = writer.get_extra_info("peername")
+    print(f"> listening on this address: {addr}")
 
     try:
         while True:
-            events = S.select()
-            for key, _ in events:
-                callback = key.data
-                callback(key.fileobj)
+            data = await reader.read(100)
+            if not data:
+                break
+
+            message = data.decode().strip()
+            print(f"> on {addr}, said...data: {message}")
+            response = f"You said: {message} \n"
+            writer.write(response.encode())
+            await writer.drain()
+
+    except ConnectionResetError:
+        pass
 
     except KeyboardInterrupt:
-        print("\n\r>> absolutely shutting down!")
+        writer.close()
+        reader.close()
+        await writer.wait_closed()
+        await reader.wait_closed()
+
     finally:
-        server_socket.close()
+        print(f"> closed connection on address {addr}")
+        writer.close()
+        await writer.wait_closed()
+
+
+async def main():
+    server = await asyncio.start_server(handle_client, "127.0.0.1", 8080)
+    addr = server.sockets[0].getsockname()
+    print(f":> started server with address: {addr}")
+
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    run_server()
+    try:
+        asyncio.run(main())
+
+    except KeyboardInterrupt:
+        print("\n\r> absolutely shutting down the server")
